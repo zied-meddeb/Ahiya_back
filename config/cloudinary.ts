1c7f289b5315aeac03c5b0c33c03904f94c3e900
unknown <malek.ben.hamadi@outlook.com>
2025-07-20 15:01:22 +0000

    changed end for cloudinary

--- config/cloudinary.ts
+++ config/cloudinary.ts
@@ -5,9 +5,9 @@ dotenv.config();
 
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME || 'Root', 
-  api_key: process.env.CLOUDINARY_API_KEY || '265763257689168',
-  api_secret: process.env.CLOUDINARY_API_SECRET || 'EW7aFQB-yewdm8q2DA1bGd9wGXE',
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME , 
+  api_key: process.env.CLOUDINARY_API_KEY ,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 export default cloudinary;
